ee42c018c40380dbedef2a233b383bb3d30042a0
Rubyn Johnson <165520287+coachrubyn@users.noreply.github.com>
2026-01-14 07:57:42 -0600

    Quick Fix

--- ou-football-dashboard/app/page.tsx
+++ ou-football-dashboard/app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from 'next/navigation'
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  redirect('/team')
+  const router = useRouter()
+  
+  useEffect(() => {
+    router.push('/team')
+  }, [router])
+
+  return null
 }
